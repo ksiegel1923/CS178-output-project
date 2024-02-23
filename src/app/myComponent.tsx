6d55cd35757a9d9fb4e3dca45e46ef200bef1950
Kara Siegel <karasiegel@college.harvard.edu>
2024-02-23 11:47:24 -0500

    Revert "initial google api setup"
    This reverts commit a96e225bc213271405450bc2cda193e543322dc5.

--- src/app/myComponent.tsx
+++ src/app/myComponent.tsx
@@ -1,31 +1,27 @@
-"use client";
-import React, { useMemo } from "react";
-import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
-
-import { useEffect, useRef } from "react";
+import React from "react";
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
 
 const containerStyle = {
   width: "400px",
   height: "400px",
 };
 
-// const center = {
-//   lat: -3.745,
-//   lng: -38.523,
-// };
+const center = {
+  lat: -3.745,
+  lng: -38.523,
+};
 
 function MyComponent() {
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
     googleMapsApiKey: "API_Key",
   });
-  const center = useMemo(() => ({ lat: 18.52043, lng: 73.856743 }), []);
 
   const [map, setMap] = React.useState(null);
 
   const onLoad = React.useCallback(function callback(map: any) {
     // This is just an example of getting and using the map instance!!! don't just blindly copy!
-    const bounds = new window.google.maps.LatLngBounds();
+    const bounds = new window.google.maps.LatLngBounds(center);
     map.fitBounds(bounds);
 
     setMap(map);
@@ -44,7 +40,6 @@ function MyComponent() {
       onUnmount={onUnmount}
     >
       {/* Child components, such as markers, info windows, etc. */}
-      <Marker position={{ lat: 18.52043, lng: 73.856743 }} />
       <></>
     </GoogleMap>
   ) : (
@@ -53,33 +48,3 @@ function MyComponent() {
 }
 
 export default React.memo(MyComponent);
-// function MyComponent({
-//   center,
-//   zoom,
-// }: {
-//   center: google.maps.LatLngLiteral;
-//   zoom: number;
-// }) {
-//   const ref = useRef<HTMLDivElement | null>(null);
-
-//   useEffect(() => {
-//     // Display the map
-//     if (ref.current) {
-//       new window.google.maps.Map(ref.current, {
-//         center: center,
-//         zoom: zoom,
-//       });
-//     }
-//   }, [ref, center, zoom]);
-
-//   return (
-//     <span>
-//       <div
-//         ref={ref}
-//         id="map"
-//         style={{ width: "1000px", height: "700px" }}
-//       ></div>
-//     </span>
-//   );
-// }
-// export default React.memo(MyComponent);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,8 @@
-"use client";
 import Image from "next/image";
 import styles from "./page.module.css";
 import MyComponent from "./myComponent";
-import { createRoot } from "react-dom/client";
-import { Wrapper } from "@googlemaps/react-wrapper";
-import ReactDOM from "react-dom";
 
 export default function Home() {
-  const center = { lat: -34.397, lng: 150.644 };
-  const zoom = 4;
   return (
     <main className={styles.main}>
       <MyComponent></MyComponent>
